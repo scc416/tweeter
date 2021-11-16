3572821817141a29f33c5daccb5053ffd72db137
Siu <siuc@protonmail.com>
2021-11-16 18:35:50 +0000

    implement dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = (tweetObj) => {
-    const { user, content, created_at } = tweetObj;
+const createTweetElement = (tweet) => {
+    const { user, content, created_at } = tweet;
     const { name, avatars, handle } = user;
     const { text } = content;
     const timePass = timeago.format(created_at);
@@ -39,23 +39,43 @@ const createTweetElement = (tweetObj) => {
     return $tweet;
 };
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
+
+
+const renderTweets = function(tweets) {
+  for (const key in tweets) {
+    const info = tweets[key];
+    const $tweet = createTweetElement(info);
+    $('#tweets-container').append($tweet);
+  }
+}
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
     "user": {
       "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
     "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
     "created_at": 1461116232227
- }
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
 $(document).ready( () => {
-    $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
+ // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
