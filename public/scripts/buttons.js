58e2f193e81d020921a3f5f285498e5bfefd6024
Siu <siuc@protonmail.com>
2021-11-17 20:41:29 +0000

    add comments

--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -4,37 +4,48 @@ $(document).ready(() => {
   const $tweetText = $("#tweet-text");
   const $scrollToTopButton = $("#scroll-to-top");
 
+  // to show(slide down) or hide(slide up) the tweet form when $toNewTweetButton is clicked
   $toNewTweetButton.on("click", () => {
+
+    // check if the form is displayed in order to decide what to do (show/hide) with the form
     const formIsDisplayed = !$newTweet.is(":hidden");
 
     if (formIsDisplayed) return $newTweet.slideUp(300);
     if (!formIsDisplayed) {
       $newTweet.slideDown(600, () => {
+
+        // after the form is display, focus on the text area of the form
         $tweetText.focus();
       });
     } 
   });
 
+  // get back to the top and show the tweet form (if it was hidden) when $scrollToTopButton is clicked
   $scrollToTopButton.on("click", () => {
     $(document).scrollTop(0);
 
     const formIsDisplayed = !$newTweet.is(":hidden");
-    
+    // display the form if it is not displayed
     if (!formIsDisplayed) {
       $newTweet.slideDown(600, () => {
+
+        // after the form is display, focus on the text area of the form
         $tweetText.focus();
       });
     }
   });
 
+  // helper function to display one button and hide another one
   const showAndHideButtons = (elm1, elm2) => {
     elm1.show();
     elm2.hide();
   };
 
+  // show one button and hide another one based on the position from the top of the page
   $(document).on("scroll", () => {
     const positionFromTop = $(document).scrollTop();
     const hideScrollToTopButton = positionFromTop < 100;
+
     if (hideScrollToTopButton) {
       showAndHideButtons($toNewTweetButton, $scrollToTopButton);
     }
